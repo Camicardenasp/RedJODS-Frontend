17720cf2f65348d97c59ad140f492ba68e8041c8
Camilo Cárdenas <camicardenasp@gmail.com>
2023-03-04 18:08:18 -0500

    Changed styles, and added Poppins.

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -4,6 +4,7 @@ import home from '/Home.png'
 export default function Home() {
     return (
         <div>
+            <h1 style={{padding: "40px"}}>¡Hola Súper Administrador!</h1>
             <img src={home} alt="" style={{ maxWidth: "100vw" }} />
         </div>
     )
